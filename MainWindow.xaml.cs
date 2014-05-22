6c5bbc14db144f22db1374b1e8486b433a1b2c76
Scthe <marcin1113C@gmail.com>
2014-05-22 21:36:12 +0200

    Base window layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AI_4 {
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+		/*
+		 canvas contains children - remove children of type lines etc.
+		 */
+
+		private readonly string dataDir;
+
 		public MainWindow() {
 			InitializeComponent();
+			var a = Directory.GetCurrentDirectory();
+			dataDir = a.Substring(0, a.LastIndexOf("bin")) + "data\\";
+			Console.WriteLine("[Info] DataDir '" + dataDir + "'");
 		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+			this.Left = 0;// desktopWorkingArea.Right - this.Width;
+			this.Top = desktopWorkingArea.Bottom - this.Height;
+
+			//drawImage("sintel_render.png", Image2);
+			//drawImage("6.jpg", Image2);
+		}
+
+
+		private void drawImage(string imgName, Image target) {
+			//http://stackoverflow.com/questions/12866758/placing-bitmap-in-canvas-in-c-sharp
+			var path = dataDir + imgName;
+			if (File.Exists(path)) {
+				//Console.WriteLine("found '" + Directory.GetCurrentDirectory() + "'");
+				BitmapImage bitmap = new BitmapImage();
+				bitmap.BeginInit();
+				bitmap.UriSource = new Uri(path);
+				bitmap.EndInit();
+				target.Source = bitmap;
+			} else {
+				Console.WriteLine("[Error] Could not find '" + imgName + "'\nSearched in: '" + path + "'");
+			}
+
+
+		}
+
 	}
+
+
+
 }
